8b400876675e55a8c6fdeae70a62bd9ef613eb2b
Rachel Lader <RachelLader@RachelsAir.local>
2015-11-11 16:41:19 -0800

    [Feat]: Initial file setup

---
+++ app/Auth/AuthController.js
@@ -0,0 +1,5 @@
+angular.module('clipr.auth',[])
+
+.controller(){
+	
+}
---
+++ app/app.js
@@ -0,0 +1,39 @@
+//all angular routing using ui-router 
+	//check out states       ex. search
+
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cliprApp
+ * @description
+ * # cliprApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cliprApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
