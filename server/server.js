1eff8e7a0e618e80abb64fe73c3438f3039ae94f
Rachel Lader <RachelLader@RachelsAir.local>
2015-11-13 10:56:49 -0800

    [Fix]: Complete add tag and add relationship functions within server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 var Promise = require('bluebird');
 var request = require('request');
-var http= require('http');
+var http = require('http');
 
 
 var db = require('seraph')({
@@ -40,26 +40,63 @@ app.post('/user/post/storeclip', function(req, res) {
     title: req.body.title
   }, function(err, node) {
     if (err) throw err;
-    db.label(node, ['Clip'],
-      function(err) {
-        if (err) throw err;
-        console.log(node.clipUrl + "was inserted into DB");
-        createWatsonUrl(node.clipUrl)
+    db.label(node, ['Clip'], function(err) {
+      if (err) throw err;
+      console.log(node.clipUrl + "was inserted as a Clip into DB");
+      createWatsonUrl(node.clipUrl, function(keywords) {
+        for (var i = 0; i < 3; i++) {
+          storeTags(keywords[i], function(tagNode) {
+              createRelation(node, tagNode);
+            })
+          }
       })
+    })
   })
 })
 
-var createWatsonUrl = function(url) {
+
+var createRelation = function(clip, tag) {
+  console.log('clip:', clip)
+  console.log('tag:', tag)
+  db.relate(clip, 'contains', tag, {
+    relevance: tag.relevance
+  }, function(err, relationship) {
+    console.log('RELATIONSHIP:', relationship)
+  })
+}
+
+
+
+var createWatsonUrl = function(url, cb) {
   console.log('inside watson')
   var API = '5770c0482acff843085443bfe94677476ed180e5'
   var baseUrl = 'http://gateway-a.watsonplatform.net/calls/'
   var endUrl = 'url/URLGetRankedKeywords?apikey=' + API + '&outputMode=json&url='
   var fullUrl = baseUrl + endUrl + url
   console.log(fullUrl)
-  request(fullUrl,  function(err, response, body){
-  	console.log('RESPONSE:',JSON.parse(body));
-  	response.end()
+  request(fullUrl, function(err, response, body) {
+    var bodyParsed = JSON.parse(body);
+    console.log('WATSON KEYWORDS:', bodyParsed.keywords)
+    cb(bodyParsed.keywords)
   })
+}
+
+var storeTags = function(tag, cb) {
+  console.log('in storeTags')
+  db.save({
+    tagName: tag.text
+      // relevance: tag.relevance
+  }, function(err, node) {
+    if (err) throw err;
+    db.label(node, ['Tag'],
+      function(err) {
+        if (err) throw err;
+        console.log(node.tagName + " was inserted as a Topic into DB")
+        console.log('TAGNODE:', node)
+      })
+    cb(node)
+  })
+
 }
 
 
