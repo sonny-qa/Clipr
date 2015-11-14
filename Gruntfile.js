779dcef44d15aba4a0ccb2cbfe522c0a6757070d
Chris Puskar <cjpuskar@comcast.net>
2015-11-13 19:59:15 -0800

    [fix]: Revert to barebones gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function(grunt) {
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+  
 
   jshint: {
     options: {
@@ -8,7 +10,9 @@ module.exports = function(grunt) {
     },
     build: ['Gruntfile.js']
   }
+
   });
+
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.registerTask('test', ['jshint']);
