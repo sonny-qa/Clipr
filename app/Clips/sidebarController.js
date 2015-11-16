03ea99c0e991ab48167cebddf65ce1423dde8e11
Chris Puskar <cjpuskar@comcast.net>
2015-11-15 17:15:24 -0800

    [chore]: Fix merge conflicts

--- app/Clips/sidebarController.js
+++ app/Clips/sidebarController.js
@@ -1,4 +1,8 @@
-angular.module('clipr.sidebar',['ui.router']);
+angular.module('clipr.sidebar',['ui.router'])
+
+.controller('SidebarController', function($scope){
+  console.log("placeholder to make linter happy");
+});
 
 
 
