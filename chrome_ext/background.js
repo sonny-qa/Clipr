b4d6b632d9c4f89297ffc9e11fa9f747a23656ab
Chris Puskar <cjpuskar@comcast.net>
2015-11-16 10:38:55 -0800

    [feat]: Get users existing bookmarks

--- chrome_ext/background.js
+++ chrome_ext/background.js
@@ -1,6 +1,5 @@
 // POST data to server using XMLHttpRequest
 function sendBookmark(url, title) {
-
   var params = '?url=' + url + '&title=' + title;
 
   // The URL to post our data to
@@ -14,7 +13,29 @@ function sendBookmark(url, title) {
   xhr.send(params);
 }
 
-// On click, get open tabs url.
+// On Install, get all bookmarks
+chrome.runtime.onInstalled.addListener(function() {
+  console.log("bookmark search exporter extension installed");
+  var bm_urls = new Array();
+
+  function fetch_bookmarks(parentNode) {
+    parentNode.forEach(function(bookmark) {
+      if(!(bookmark.url === undefined || bookmark.url === null)) {
+        bm_urls.push(bookmark.url);
+      }
+      if(bookmark.children) {
+        fetch_bookmarks(bookmark.children);
+      }
+    });
+  }
+
+  chrome.bookmarks.getTree(function(rootNode) {
+    fetch_bookmarks(rootNode);
+    console.log(JSON.stringify(bm_urls));
+  });
+});
+
+// On click, get open tabs url
 chrome.browserAction.onClicked.addListener(function(tab) {
   var tabUrl = tab.url;
   var tabTitle = tab.title;
