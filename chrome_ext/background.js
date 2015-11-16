45a69971ef1f277731a8b47b643fc75b23244627
Chris Puskar <cjpuskar@comcast.net>
2015-11-16 11:22:11 -0800

    [feat]: Add function to grab users existing bookmarks

--- chrome_ext/background.js
+++ chrome_ext/background.js
@@ -1,3 +1,7 @@
+//*******************************************************************
+// Helper Functions
+//*******************************************************************
+
 // POST data to server using XMLHttpRequest
 function sendBookmark(url, title) {
   var params = '?url=' + url + '&title=' + title;
@@ -13,11 +17,29 @@ function sendBookmark(url, title) {
   xhr.send(params);
 }
 
+// Send all existing bookmarks to server
+function sendAllBookmarks(bookmarkObj) {
+  console.log('placeholder');
+}
+
+//*******************************************************************
+// Event Listeners
+//*******************************************************************
+
+// On click, get open tabs url
+chrome.browserAction.onClicked.addListener(function(tab) {
+  var tabUrl = tab.url;
+  var tabTitle = tab.title;
+  console.log("tabUrl: ", tabUrl);
+  console.log('tabTitle: ', tabTitle);
+  sendBookmark(tabUrl, tabTitle);
+});
+
 // On Install, get all bookmarks
 chrome.runtime.onInstalled.addListener(function() {
   console.log("bookmark search exporter extension installed");
-  var bm_urls = new Array();
-
+  var bm_urls = [];
+  console.log("bm_urls: ", bm_urls);
   function fetch_bookmarks(parentNode) {
     parentNode.forEach(function(bookmark) {
       if(!(bookmark.url === undefined || bookmark.url === null)) {
@@ -35,12 +57,20 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 });
 
-// On click, get open tabs url
-chrome.browserAction.onClicked.addListener(function(tab) {
-  var tabUrl = tab.url;
-  var tabTitle = tab.title;
-  console.log("tabUrl: ", tabUrl);
-  console.log('tabTitle: ', tabTitle);
-  sendBookmark(tabUrl, tabTitle);
+// Logs new bookmark url
+chrome.bookmarks.onCreated.addListener(function(id, bookmark) {
+  console.log("bookmark added.. " + bookmark.url);
 });
+
+// Logs deleted bookmark id
+chrome.bookmarks.onRemoved.addListener(function(id, removeInfo) {
+  console.log("bookmark removed.. " + id);
+});
+
+// Logs id of changed bookmark
+chrome.bookmarks.onChanged.addListener(function(id, changeInfo) {
+  console.log("bookmark changed.. " + id);
+});
+
+
 
