1d44876cd2a725013ccb7a2915a090c16b014ae4
Chris Puskar <cjpuskar@comcast.net>
2015-11-17 11:46:46 -0800

    [chore]: Refactor to use matchdep instead of many loadNpmTasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,18 +8,19 @@ module.exports = function(grunt) {
   });
 
   // Load Plugins
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-csslint');
-  grunt.loadNpmTasks('grunt-nodemon');
-  grunt.loadNpmTasks('grunt-newer');
-  grunt.loadNpmTasks('grunt-html-angular-validate');
-  grunt.loadNpmTasks('grunt-open');
-  grunt.loadNpmTasks('grunt-express');
+  // grunt.loadNpmTasks('grunt-contrib-jshint');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-concat');
+  // grunt.loadNpmTasks('grunt-contrib-clean');
+  // grunt.loadNpmTasks('grunt-contrib-watch');
+  // grunt.loadNpmTasks('grunt-contrib-cssmin');
+  // grunt.loadNpmTasks('grunt-contrib-csslint');
+  // grunt.loadNpmTasks('grunt-nodemon');
+  // grunt.loadNpmTasks('grunt-newer');
+  // grunt.loadNpmTasks('grunt-html-angular-validate');
+  // grunt.loadNpmTasks('grunt-open');
+  // grunt.loadNpmTasks('grunt-express');
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   // Project Configuration
   grunt.initConfig({
