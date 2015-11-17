823da64d2b15791420a7659243b064a2dd3cd305
Chris Puskar <cjpuskar@comcast.net>
2015-11-16 19:16:31 -0800

    [chore]: Cleaned up jshint errors

--- server/server.js
+++ server/server.js
@@ -51,7 +51,7 @@ app.post('/user/post/storeclip', function(req, res) {
     title: req.query.title
   }, function(err, node) {
     if (err) throw err;
-    console.log('clipnode', node)
+    console.log('clipnode', node);
     db.label(node, ['Clip'], function(err) {
       if (err) throw err;
       console.log(node + " was inserted as a Clip into DB");
@@ -59,11 +59,11 @@ app.post('/user/post/storeclip', function(req, res) {
         for (var i = 0; i < 3; i++) {
           storeTags(keywords[i], function(tagNode, relevance) {
             createRelation(node, tagNode, relevance, 'contains');
-          })
+          });
         }
-      })
-    })
-  })
+      });
+    });
+  });
 });
 
 app.get('/loadClips', function(req, res) {
@@ -74,8 +74,8 @@ app.get('/loadClips', function(req, res) {
 });
 
 app.post('/user/post/addNote', function(req, res) {
-  console.log('in addNote')
-  console.log('url', req.query.url)
+  console.log('in addNote');
+  console.log('url', req.query.url);
     // console.log('url', req.query.user)
 
   var clipNode;
@@ -85,60 +85,60 @@ app.post('/user/post/addNote', function(req, res) {
     if (err) throw err;
     clipNode = clip;
   });
-  console.log(req.query.note)
+  console.log(req.query.note);
   db.save({
     note: req.query.note
   }, function(err, noteNode) {
-    console.log(' note was saved', noteNode)
+    console.log(' note was saved', noteNode);
     if (err) throw err;
     db.label(noteNode, ['Note'], function(err, labeledNode) {
       if (err) throw err;
-      console.log('noteNode', labeledNode)
-      console.log('clipNode', clipNode)
-    })
+      console.log('noteNode', labeledNode);
+      console.log('clipNode', clipNode);
+    });
     createRelation(noteNode, clipNode, 3, 'belongsTo');
     // createRelation(userNode, noteNode, 3, 'owns');
-  })
+  });
 });
 
 app.post('/user/post/loadNotes', function(req, res) {
-  console.log('inloadnotes')
+  console.log('inloadnotes');
 
-  var cypher = "MATCH(notes)-[:belongsTo]->(clip) WHERE clip.clipUrl='" + req.query.url + "' RETURN notes"
+  var cypher = "MATCH(notes)-[:belongsTo]->(clip) WHERE clip.clipUrl='" + req.query.url + "' RETURN notes";
 
   db.query(cypher, function(err, result) {
     if (err) throw err;
-    console.log('NOTESRESULT', result)
-  })
+    console.log('NOTESRESULT', result);
+  });
 });
 
 // DB HELPER FUNCTIONS
 var createRelation = function(clip, tag, relevance) {
-  console.log('clip:', clip)
-  console.log('tag:', tag)
+  console.log('clip:', clip);
+  console.log('tag:', tag);
   db.relate(clip, how, tag, {
     relevance: relevance
   }, function(err, relationship) {
-    console.log('RELATIONSHIP:', relationship)
-  })
+    console.log('RELATIONSHIP:', relationship);
+  });
 };
 
 var createWatsonUrl = function(url, cb) {
-  console.log('inside watson')
-  var API = '5770c0482acff843085443bfe94677476ed180e5'
-  var baseUrl = 'http://gateway-a.watsonplatform.net/calls/'
-  var endUrl = 'url/URLGetRankedKeywords?apikey=' + API + '&outputMode=json&url='
-  var fullUrl = baseUrl + endUrl + url
-  console.log(fullUrl)
+  console.log('inside watson');
+  var API = '5770c0482acff843085443bfe94677476ed180e5';
+  var baseUrl = 'http://gateway-a.watsonplatform.net/calls/';
+  var endUrl = 'url/URLGetRankedKeywords?apikey=' + API + '&outputMode=json&url=';
+  var fullUrl = baseUrl + endUrl + url;
+  console.log(fullUrl);
   request(fullUrl, function(err, response, body) {
     var bodyParsed = JSON.parse(body);
-    console.log('WATSON KEYWORDS:', bodyParsed.keywords)
+    console.log('WATSON KEYWORDS:', bodyParsed.keywords);
     cb(bodyParsed.keywords)
-  })
+  });
 };
 
 var storeTags = function(tag, cb) {
-  console.log('in storeTags')
+  console.log('in storeTags');
   var relevance = tag.relevance;
   db.save({
     tagName: tag.text
@@ -147,11 +147,11 @@ var storeTags = function(tag, cb) {
     db.label(node, ['Tag'],
       function(err) {
         if (err) throw err;
-        console.log(node.tagName + " was inserted as a Topic into DB")
-        console.log('TAGNODE:', node)
-      })
+        console.log(node.tagName + " was inserted as a Topic into DB");
+        console.log('TAGNODE:', node);
+      });
     cb(node, relevance)
-  })
+  });
 };
 
 app.listen(port);
