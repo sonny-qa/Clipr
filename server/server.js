d052206de319f323bb0eaea81876869b1eb1439a
Chris Puskar <cjpuskar@comcast.net>
2015-11-17 19:16:10 -0800

    [chore]: moved middleware above routes

--- server/server.js
+++ server/server.js
@@ -27,8 +27,22 @@ var db = require('seraph')({
   pass: 'oSvInWIWVVCQIbxLbfTu'
 });
 
+// CONFIG SERVER
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(__dirname + '../../app'));
+// Set Response Headers
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(compression());
 
 /**
   Google OAuth2
@@ -63,22 +77,6 @@ passport.deserializeUser(function (obj, done) {
   //   done(err, user);
   // });
 });
-
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-app.use(express.static(__dirname + '../../app'));
-
-// Set Response Headers
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use(compression());
 
 // ROUTES
 app.get('/auth/google', 
