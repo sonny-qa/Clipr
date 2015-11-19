179e18c30780ecabcc46b2ccc3955b50ef32fbbd
Rachel Lader <RachelLader@RachelsAir.local>
2015-11-18 20:11:21 -0800

    [style]: Changes to landing page and clipView

--- chrome_ext/background.js
+++ chrome_ext/background.js
@@ -2,110 +2,6 @@
 // Helper Functions
 //*******************************************************************
 
-<<<<<<< HEAD
-// POST existing chrome bookmarks to server
-function sendAllBookmarks(bookmarkObj) {
-
-  // The URL to post our data to
-  var postUrl = "http://localhost:3000/user/post/getAllBookmarks";
-
-  // Set up an async POST Request
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', postUrl, true);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-
-  // Convert to JSON
-  var sendData = JSON.stringify(bookmarkObj);
-
-  // console.log("Im the bookmark obj: ", bookmarkObj);
-  // Send the request
-  xhr.send(bookmarkObj);
-}
-
-//*******************************************************************
-// Event Listeners
-//*******************************************************************
-// POST url to server using XMLHttpRequest
-function sendBookmark(url, title, imgUrl) {
-  console.log('SEND BOOKMARK FUNCTION BEING CALLED');
-  console.log('imgUrl', imgUrl);
-
-  var params = '?url=' + url + '&title=' + title;
-  // The URL to post our data to
-  var postUrl = "http://localhost:3000/user/post/storeclip" + params;
-
-  // Set up an async POST Request
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', postUrl, true);
-  // xhr.setRequestHeader('Content-Type', 'application/json');
-  // Send the request
-  xhr.send(params);
-}
-
-// On Install, get all chrome bookmarks
-
-// chrome.runtime.onInstalled.addListener(function() {
-  
-//   var bm_urls = [];
-
-//   function fetch_bookmarks(parentNode) {
-//     parentNode.forEach(function(bookmark) {
-//       if(!(bookmark.url === undefined || bookmark.url === null)) {
-//         bm_urls.push(bookmark.url);
-//       }
-//       if(bookmark.children) {
-//         fetch_bookmarks(bookmark.children);
-//       }
-//     });    
-//   }
-
-//   chrome.bookmarks.getTree(function(rootNode) {
-//     fetch_bookmarks(rootNode);
-//     console.log("Stringifed Array: ", JSON.stringify(bm_urls));
-//     sendAllBookmarks(JSON.stringify(bm_urls));
-//   });
-// });
-
-// On click, get open tabs url
-chrome.browserAction.onClicked.addListener(function(tab) {
-
-  console.log('Inside Click Handler!');
-  var tabUrl = tab.url;
-  var tabTitle = tab.title;
-
-  chrome.windows.getCurrent(function(win) {
-  console.log('INSIDE getCurrent');
-    chrome.tabs.captureVisibleTab(win.id, {"format": "png"}, function(imgUrl) {
-      console.log('PREVIEW IMAGE URL : ===============');
-      console.log(imgUrl);
-      var tabImgUrl = imgUrl;
-      console.log('INSIDE CHROME EXT CALLBACK');
-      sendBookmark(tabUrl, tabTitle, tabImgUrl);
-    });
-  });
-
-  console.log("tabUrl: ", tabUrl);
-  console.log('tabTitle: ', tabTitle);
-
-});
-
-
-
-// Logs new bookmark url
-chrome.bookmarks.onCreated.addListener(function(id, bookmark) {
-  console.log("bookmark added.. " + bookmark.url);
-});
-
-// Logs deleted bookmark id
-chrome.bookmarks.onRemoved.addListener(function(id, removeInfo) {
-  console.log("bookmark removed.. " + id);
-});
-
-// Logs id of changed bookmark
-chrome.bookmarks.onChanged.addListener(function(id, changeInfo) {
-  console.log("bookmark changed.. " + id);
-});
-=======
 
 //listener for sending in new bookmark
 chrome.browserAction.onClicked.addListener(function(tab) {
@@ -160,6 +56,7 @@ var checkAuth = new Promise(function(resolve, reject) {
 //--------sends creates a bookmark from the current tab & sends to server. expects user email
 function sendBookmark(bkmrkObj) {
 
+    //NOTE change this to https://clipr-app-1.herokuapp.com for heroku
     var website = "http://localhost:3000";
     var postUrl = website + "/user/post/storeclip";
 
@@ -186,9 +83,9 @@ var getPageImg = function(bkmrkObj,cb) {
         }, function(imgUrl) {
 
             //disabled this for now
-            bkmrkObj.imgUrl = "";
+            bkmrkObj.imgUrl = imgUrl;
           
-            
+            //callback for sending bookmark
             cb(bkmrkObj)
 
         });
@@ -199,7 +96,6 @@ var getPageImg = function(bkmrkObj,cb) {
 };
 
 
->>>>>>> [chore]: Refactor chrome_ext to move sendBookmark function into popup.js
 
 
 
