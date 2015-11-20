26c75d5514d7a36a53fe53a3c565d57760211206
Alex Kim <minseoalexk@gmail.com>
2015-11-19 17:28:18 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -104,24 +104,6 @@ passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
-
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-app.use(express.static(__dirname + '../../app'));
-
-// Set Response Headers
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.use(compression());
-
 // ROUTES
 
 app.get('/auth/google',
