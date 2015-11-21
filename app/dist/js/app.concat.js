4aaef2b9c6c0b2bec7e5ad2b1d86e965bb65c09d
Rachel Lader <RachelLader@RachelsAir.local>
2015-11-21 09:54:21 -0800

    [Feat]: Begin slide in menu"

--- app/dist/js/app.concat.js
+++ app/dist/js/app.concat.js
@@ -2,21 +2,31 @@ angular.module('clipr.auth',[])
 
 .controller('AuthController', function($scope){
  // console.log("hello");
-}); ;;angular.module('clipr.clipped',['ui.router', 'ui.bootstrap'])
+}); ;angular.module('clipr.clipped', ['ui.router', 'ui.bootstrap', 'ngAside'])
 
-.controller('ClipController',['$scope', 'Clips', '$modal', 'Notes', 'AuthService', function($scope, Clips, $modal, Notes, AuthService){
+.controller('ClipController', ['$scope', 'Clips', '$modal', 'Notes', 'AuthService', '$aside', function($scope, Clips, $modal, Notes, AuthService, $aside) {
 
   $scope.clips = Clips.clips;
+  $scope.clipShow= false;
 
-  $scope.loadAllClips= function (){
-    Clips.loadAllClips()
+  $scope.loadAllClips = function() {
+    Clips.loadAllClips();
   };
 
   $scope.loadAllClips();
 
-  $scope.logOut = function(){
+  $scope.logOut = function() {
     AuthService.logOut();
   };
+
+  $scope.clipToggle = function() {
+  	if($scope.clipShow===false){
+  		$scope.clipShow=true;
+  	}
+  	if($scope.clipShow===true){
+  		$scope.clipShow=false;
+  	}
+  }
 
   $scope.showModal = function(clipIndex, size) {
     $scope.opts = {
@@ -25,57 +35,77 @@ angular.module('clipr.auth',[])
       backdropClick: true,
       dialogFade: false,
       keyboard: true,
-      templateUrl : './clipSelect/clipSelectView.html',
-      controller : ModalInstanceCtrl,
+      templateUrl: './clipSelect/clipSelectView.html',
+      controller: ModalInstanceCtrl,
       resolve: {}
     };
 
     $scope.opts.resolve.item = function() {
-      return angular.copy({clipUrl:$scope.clips.data[clipIndex].clipUrl}); // pass name to Dialog
+      return angular.copy({
+        clipUrl: $scope.clips.data[clipIndex].clipUrl
+      }); // pass name to Dialog
     };
 
     var modalInstance = $modal.open($scope.opts);
-        modalInstance.result.then(function(){
-              //on ok button press
-            },function(){
-              //on cancel button press
-              console.log("Modal Closed");
-            });
+    modalInstance.result.then(function() {
+      //on ok button press
+    }, function() {
+      //on cancel button press
+      console.log("Modal Closed");
+    });
   };
+
+  $scope.openAside = function(position) {
+    console.log('inside asiiiiideee');
+    $aside.open({
+      templateUrl: './Suggestions/categorySuggestionsView.html',
+      placement: position,
+      backdrop: false,
+      controller: function($scope, $modalInstance) {
+        $scope.ok = function(e) {
+          $modalInstance.close();
+          e.stopPropagation();
+        };
+        $scope.cancel = function(e) {
+          $modalInstance.dismiss();
+          e.stopPropagation();
+        };
+      }
+    })
+  }
 
 }]);
 
 var ModalInstanceCtrl = function($scope, $modalInstance, $modal, item, $sce, Notes) {
 
   $scope.item = item;
-  $scope.sceUrl= $sce.trustAsResourceUrl($scope.item.clipUrl);
-  $scope.notes= Notes.notesObj;
+  $scope.sceUrl = $sce.trustAsResourceUrl($scope.item.clipUrl);
+  $scope.notes = Notes.notesObj;
 
-  $scope.ok = function () {
+  $scope.ok = function() {
     $modalInstance.close();
   };
 
-  $scope.cancel = function () {
+  $scope.cancel = function() {
     $modalInstance.dismiss('cancel');
   };
 
   //On 'save', make call to server with notes and site url
   //fetch Notes and display it
-  $scope.save = function(userNotes){
-      $scope.NoteAndUrl = {
-        note : userNotes,
-        url : $scope.item.clipUrl
-      };
-      console.log('Notes being passed to server', $scope.NoteAndUrl);
-      Notes.addNotes($scope.NoteAndUrl);
+  $scope.save = function(userNotes) {
+    $scope.NoteAndUrl = {
+      note: userNotes,
+      url: $scope.item.clipUrl
+    };
+    console.log('Notes being passed to server', $scope.NoteAndUrl);
+    Notes.addNotes($scope.NoteAndUrl);
   };
 
-  $scope.display = function(){
+  $scope.display = function() {
     console.log('display function!!!');
     Notes.loadNotes($scope.item.clipUrl);
   };
-};
-;angular.module('clipr.header',['ui.router']);
+};;angular.module('clipr.header',['ui.router']);
 
 
 
@@ -83,11 +113,10 @@ var ModalInstanceCtrl = function($scope, $modalInstance, $modal, item, $sce, Not
 ;angular.module('clipr.sidebar',['ui.router'])
 
 .controller('SidebarController',['$scope', 'Clips', function($scope, Clips){
-  console.log("placeholder to make linter happy");
 
   $scope.loadClipsByCategory= function(category){
   	Clips.loadClipsByCategory(category);
-  }
+  };
 
 }]);
 
