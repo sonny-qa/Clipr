3741405bf67b5f0680b1e07af5d2209a85418523
Rachel Lader <RachelLader@RachelsAir.local>
2015-11-30 20:02:39 -0800

    [chore]Train Naive Bayes classifier

--- server/config/classify.js
+++ server/config/classify.js
@@ -8,15 +8,9 @@ var async = require('async');
 
 
 
-// // // techStream.on('end', function() {
-// //   classifier.addDocument(data, 'Science/Technology')
-// //   classifier.train();
-// // });
+// var files = ['tech.txt', 'news.txt', 'entertainment.txt', 'politics.txt', 'food.txt', 'sports.txt', 'travel.txt', 'business.txt', 'shopping.txt']
 
-// // 'sports.txt', 'entertainment.txt', 'business.txt', 
-// var files = ['tech.txt', 'news.txt']
-//   // 'Sports', 'Entertainment', 'Business', 
-// var labels = ['Science/Technology', 'News']
+// var labels = ['Science/Technology', 'News', 'Entertainment', 'Politics', 'Food', 'Sports', 'Travel', 'Business', 'Shopping']
 
 // var readAsync = function(file, callback) {
 //   longFile = './server/config/trainingDocs/' + file
@@ -33,27 +27,19 @@ var async = require('async');
 
 // })
 
-// classifier.addDocument('basketball baseball', 'Sports');
-// classifier.addDocument('movies art', 'Entertainment');
-// classifier.addDocument('chemistry, computers, biology', 'Science/Technology');
-// classifier.addDocument('money, start up, business', 'Business');
-// classifier.addDocument('terrorists, world, america', 'News');
-// classifier.addDocument('learning, class, school', 'Education');
-// classifier.addDocument('love, children, home', 'Family');
-// classifier.addDocument('recipe, pizza, drinks, restaurant', 'Food');
-// classifier.addDocument('running, fitness, health', 'Health');
-// classifier.addDocument('dog, cat, bird, pet', 'Pets');
-// classifier.addDocument('democrat, republican, trump, clinton', 'Politics');
-// classifier.addDocument('airplane, flight, hotel, car, vacation', 'Travel');
-// classifier.addDocument('buy, clothing, store', 'Shopping');
+
+// // classifier.addDocument('learning, class, school', 'Education');
+// // classifier.addDocument('love, children, home', 'Family');
+// // classifier.addDocument('running, fitness, health', 'Health');
+
+
 
 module.exports = {
 
   loadClassifier: function() {
     natural.BayesClassifier.load('classifier.json', null, function(err, classifier) {
-      console.log(classifier.classify('computers are good i like technology'))
-      console.log(classifier.classify('i love news hilary clinton'))
-    })
+      console.log(classifier.getClassifications("SYRACUSE, N.Y. -- Onondaga Community Colleges men's basketball team will play its second game tomorrow since their coach was placed on leave pending the outcome of an investigation into allegations of unspecified misconduct.A former player and a former assistant coach say Dave Pasiak has been a man of high integrity over the 16 seasons he's been OCC's head coach.I dont see any reason for him being on leave, said Tyrone Albright, who was the two-year captain of the team from 2000 to 2002. Hes always been socially conscious. He loves everyone. As long as you have the basketball skills and you're doing what you're supposed to be doing in the classroom, he'll give you the opportunity to wear that jersey."))
+});
   },
 
   trainClassifier: function() {
