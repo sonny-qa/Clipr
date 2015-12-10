620685ac1e43d26e13c52dec3a4221f9411c15c2
Chris Puskar <cjpuskar@comcast.net>
2015-12-09 17:23:21 -0800

    [chore]: delete duplicate npm modules

--- server/config/paths.js
+++ server/config/paths.js
@@ -42,8 +42,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-
 app.use(session({
   secret: 'this is a secret',
   resave: true,
