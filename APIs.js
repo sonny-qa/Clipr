1b2f48b38620239d6b18998fbadef9409606c27d
Chris Puskar <cjpuskar@comcast.net>
2015-12-12 14:52:53 -0800

    [docs]: Add chrome store image

--- APIs.js
+++ APIs.js
@@ -1,7 +1,9 @@
 module.exports = {
+  
   // Used in Google OAuth - server.js
   clientID: '956444297317-c7q8o48o6trac3u2c81l5q6vf31r30up.apps.googleusercontent.com',
   clientSecret: 'reN8EHttjTzrGmvC6_C4oivR',
+
   // Used in Initialize DB CONNECTION - utils.js
   dbServerUrl: "http://clipr.sb02.stations.graphenedb.com:24789",
   dbUser: 'clipr',
@@ -9,10 +11,12 @@ module.exports = {
   
   // FAROO API key - utils.js
   FAROO: '&key=nwFFIfopV8GKlLMLC3tJU9Nc6rg_',
+
   // Cloudinary - utils.js
   cloudName: 'cjpuskar',
   cloudinary_api_key: '499291937259717',
   cloudinary_api_password: 'eaGBQyaTw9EKtPG351ZrkTmTMWc',
+
   // WATSON API - utils.js
   watsonAPI: '5770c0482acff843085443bfe94677476ed180e5'
 };
