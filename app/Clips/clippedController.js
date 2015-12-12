72335f84920b0e65359bb025a72e05a795e12449
Rachel Lader <RachelLader@RachelsAir.local>
2015-12-12 15:30:41 -0800

    [fix] fix merge issue

--- app/Clips/clippedController.js
+++ app/Clips/clippedController.js
@@ -122,73 +122,52 @@ angular.module('clipr.clipped', ['ui.router', 'ui.bootstrap', 'ngAside', 'angula
     });
   };
 
-  $scope.openAside = function(position) {
-    console.log('inside asiiiiideee');
-    $aside.open({
-      templateUrl: 'html/categorySuggestionsView.html',
-      placement: position,
-      backdrop: false,
-      controller: function($scope, $modalInstance) {
-        $scope.ok = function(e) {
-          $modalInstance.close();
-          e.stopPropagation();
-        };
-        $scope.cancel = function(e) {
-          $modalInstance.dismiss();
-          e.stopPropagation();
-        };
-      }
-    });
-  };
 
 }]);
 
 var ModalInstanceCtrl = function($scope, $modalInstance, Clips, $modal, item, $window) {
-    $scope.collections = Clips.clips.collections;
-    $scope.item = item.clip;
+  $scope.collections = Clips.clips.collections;
+  $scope.item = item.clip;
 
 
-    $scope.twitShare = function(clipUrl) {
-      $window.open('https://twitter.com/intent/tweet?hashtags=clippr&text=' + clipUrl, 'height=300, width=400');
-      var ModalInstanceCtrl = function($scope, $modalInstance, Clips, $modal, item) {
-        $scope.collections = Clips.clips.collections
-        $scope.item = item.clip
+  $scope.twitShare = function(clipUrl) {
+    $window.open('https://twitter.com/intent/tweet?hashtags=clippr&text=' + clipUrl, 'height=300, width=400');
+  }
+  
+  $scope.windowOpen = function(clipUrl) {
+    $window.open('https://twitter.com/intent/tweet?hashtags=clipr&text=' + clipUrl, 'height=300, width=400');
+  };
 
-      }
-      $scope.windowOpen = function(clipUrl) {
-        $window.open('https://twitter.com/intent/tweet?hashtags=clipr&text=' + clipUrl, 'height=300, width=400');
-      };
+  $scope.fbShare = function(url, title, winWidth, winHeight) {
+    var winTop = (screen.height / 4) - (winHeight / 2);
+    var winLeft = (screen.width / 4) - (winWidth / 2);
+    window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[url]=' + url + 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+  };
 
-      $scope.fbShare = function(url, title, winWidth, winHeight) {
-        var winTop = (screen.height / 4) - (winHeight / 2);
-        var winLeft = (screen.width / 4) - (winWidth / 2);
-        window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[url]=' + url + 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
-      };
+  $scope.gooShare = function(url, title, winWidth, winHeight) {
+    var w = 480;
+    var h = 380;
+    var x = Number((window.screen.width - w) / 2);
+    var y = Number((window.screen.height - h) / 2);
+    $window.open('https://plusone.google.com/share?hl=en&url=' + encodeURIComponent(url) + '&title=' + encodeURIComponent(title) + '', 'width=' + w + ',height=' + h + ',left=' + x + ',top=' + y + ',scrollbars=no');
 
-      $scope.gooShare = function(url, title, winWidth, winHeight) {
-        var w = 480;
-        var h = 380;
-        var x = Number((window.screen.width - w) / 2);
-        var y = Number((window.screen.height - h) / 2);
-        $window.open('https://plusone.google.com/share?hl=en&url=' + encodeURIComponent(url) + '&title=' + encodeURIComponent(title) + '', 'width=' + w + ',height=' + h + ',left=' + x + ',top=' + y + ',scrollbars=no');
+  };
 
-      };
+  $scope.ok = function() {
+    $modalInstance.close();
+  };
 
-      $scope.ok = function() {
-        $modalInstance.close();
-      };
+  $scope.addToCollection = function(collection, clip) {
+    Clips.addToCollection(collection, clip);
+  };
 
-      $scope.addToCollection = function(collection, clip) {
-        Clips.addToCollection(collection, clip);
-      };
+  $scope.changeCategory = function(category, clip) {
+    clip.category = category;
+    Clips.changeCategory(category, clip.title);
+  };
 
-      $scope.changeCategory = function(category, clip) {
-        clip.category = category;
-        Clips.changeCategory(category, clip.title);
-      };
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancel');
+  };
 
-      $scope.cancel = function() {
-        $modalInstance.dismiss('cancel');
-      };
-
-    };
+};
